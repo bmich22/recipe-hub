64a324cdef0b0d3c85aceb7ba22475980678036a
bmich22 <barbmichel22@gmail.com>
2025-02-04 19:51:12 +0100

    Add registration form in member app

--- member/views.py
+++ member/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-
-# def member(request):
-#     template = loader.get_template('register.html')
-#     return HttpResponse(template.render())
-
-# def register_view(request):
-#     return render(request, "member/register.html")
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
 def register_view(request):
-    template = loader.get_template('member/register.html')
-    return HttpResponse(template.render())
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("recipe:list")
+    else:
+        form = UserCreationForm
+    form = UserCreationForm()
+    return render(request, "member/register.html", {"form": form})
