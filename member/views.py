0851b9eeaad1ad11c692f8c8baccc431308ca9bc
bmich22 <barbmichel22@gmail.com>
2025-02-05 09:39:13 +0100

    Create user registration form and logic

--- member/views.py
+++ member/views.py
@@ -3,12 +3,18 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 def register_view(request):
+    # if the form has been submitted
     if request.method == "POST":
+        # then form is submitted with information
         form = UserCreationForm(request.POST)
+        # information is validated
         if form.is_valid():
+            # information about new user is saved
             form.save()
+            # send user back to recipe list page
             return redirect("recipe:list")
     else:
-        form = UserCreationForm
-    form = UserCreationForm()
+        # if form is not submitted
+        form = UserCreationForm()
+    # validation message or empty form in the case that form was not submitted
     return render(request, "member/register.html", {"form": form})
